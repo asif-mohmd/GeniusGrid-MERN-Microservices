d567cdf7ec10ba01ab40b0bea572bb2b5d0d8884
asif-mohmd <asifasifpsps@gmail.com>
2024-06-06 12:23:51 +0530

    order service update

--- Order-service/src/server.ts
+++ Order-service/src/server.ts
@@ -2,6 +2,8 @@ import { connectDB } from "./config/mongoDB/db";
 import dotenv from "dotenv"
 import RabbitMQClient from "./rabbitMQ/client"
 
+console.log("order service")
+
 dotenv.config();
 connectDB()
 
